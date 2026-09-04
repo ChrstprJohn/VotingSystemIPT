acdc44b35b5ff2191f9042463b574a52b51f1762
ChrstprJohn <ChrstprJohn@users.noreply.github.com>
2026-09-05 02:08:00 +0800

    added sidebar for admin and partylist page

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -8,6 +8,25 @@ namespace VotingSystem.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["ActivePage"] = "Dashboard";
+            return View();
+        }
+
+        public IActionResult Voters()
+        {
+            ViewData["ActivePage"] = "Voters";
+            return View();
+        }
+
+        public IActionResult Elections()
+        {
+            ViewData["ActivePage"] = "Elections";
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            ViewData["ActivePage"] = "Settings";
             return View();
         }
     }
