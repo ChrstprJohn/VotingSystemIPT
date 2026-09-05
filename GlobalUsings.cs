5fd9a2afba9bb47f74c949c5785637df51b28670
ChrstprJohn <ChrstprJohn@users.noreply.github.com>
2026-09-05 16:52:59 +0800

    fix: register AccountService and HttpContextAccessor in DI container

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,3 +3,4 @@ global using Microsoft.AspNetCore.Authentication;
 global using Microsoft.AspNetCore.Authentication.Cookies;
 global using Microsoft.AspNetCore.Mvc;
 global using VotingSystem.Models;
+global using VotingSystem.Controllers.Services;
